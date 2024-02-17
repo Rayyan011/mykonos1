583197671fb1779173d654df73910b8d0a6addda
Rayyan011 <ameezrayyan@gmail.com>
2024-02-17 21:55:58 +0500

    intial commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maldives</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="hero">
+        <video autoplay loop muted playsinline class="back-video">
+            <source src="images/video.mp4" type="video/mp4">
+        </video>
+        <nav>
+        <img src="images/logo.png" class="logo" style="width: 60px;">
+
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">HOTELS</a></li>
+                <li><a href="#">ABOUT US</a></li>
+                <li><a href="#">CONTACT US</a></li>
+            </ul>
+        </nav>
+        <div class="content">
+            <h1>Mykonos</h1>
+            <a href="">Explore</a>
+        </div>
+    </div>
+</body>
+</html>
